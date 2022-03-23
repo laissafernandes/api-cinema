cbaa93ed12f79902a84e15257fa5382787b1353a
Laíssa Fernandes <laissafermaria@gmail.com>
2022-03-22 23:36:24 -0300

    Corrigindo as validacoes

--- my_api/services/Validacoes.js
+++ my_api/services/Validacoes.js
@@ -1,27 +1,6 @@
 class Validacoes{
     
-    static validaNome(nome){
-        if(nome.length >= 3){
-            return true
-        } else {
-            console.log({erro: "nome ou sobrenome inválido"})
-            return false
-        }
-    }
     
-    static validaSobrenome(sobrenome){
-        return this.validaNome(sobrenome)
-    }
-    
-    static validaEmail(email){
-        const reg = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i
-        if(reg.test(email)){
-            return true
-        } else {
-            console.log({erro: "e-mail inválido"})
-            return false
-        }
-    }
     static validaBody(body, length){
         if(
             Object.values(body).length == length &&
